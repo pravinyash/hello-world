6fb8d5274ea621eb602b2e7772ea50adce92d5e1
Pravin <pravinyash@gmail.com>
2020-04-09 22:04:43 +0530

    THis is first java program.

---
+++ Test.java
@@ -0,0 +1,10 @@
+Class Test
+{
+	public static void main(String args[])
+	{
+		System.out.print("*********************************************");
+		System.out.print("Hello World!");
+		System.out.print("*********************************************");
+	}
+	
+}
